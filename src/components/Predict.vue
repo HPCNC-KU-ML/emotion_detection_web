9ad26e7fd3b11d0d7f675dbacec530583625697e
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-29 17:48:31 +0700

    Update predicet page

--- src/components/Predict.vue
+++ src/components/Predict.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="predict">This is predict.</div>
+  <div id="predict">
+    <h1 class="title">Upload image/video to predict.</h1>
+    <section>
+      <b-field>
+        <b-upload v-model="dropFiles" multiple drag-drop>
+          <section class="section">
+            <div class="content has-text-centered">
+              <p>
+                <b-icon icon="upload" size="is-large"></b-icon>
+              </p>
+              <p>Drop your files here or click to upload</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
+
+      <div class="tags">
+        <span v-for="(file, index) in dropFiles" :key="index" class="tag is-primary">
+          {{file.name}}
+          <button class="delete is-small" type="button" @click="deleteDropFile(index)"></button>
+        </span>
+      </div>
+    </section>
+    <button class="button is-primary" style="margin-top: 2%;">Click me!</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "predict"
+  name: "predict",
+  data() {
+    return {
+      dropFiles: []
+    };
+  },
+  methods: {
+    deleteDropFile(index) {
+      this.dropFiles.splice(index, 1);
+    }
+  }
 };
 </script>
 
 <style>
+#predict {
+  text-align: center;
+  margin-top: 3%;
+}
 </style>
