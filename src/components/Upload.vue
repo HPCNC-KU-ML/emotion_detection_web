11a0f58fa218f573b66c80c182365183eb0a6136
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-29 16:56:56 +0700

    Add upload page

---
+++ src/components/Upload.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="upload">
+    <UploadFile/>
+    <ButtonPredict/>
+  </div>
+</template>
+
+<script>
+import UploadFile from "@/components/UploadFile";
+import ButtonPredict from "@/components/ButtonPredict";
+export default {
+  name: "upload",
+  components: {
+    UploadFile,
+    ButtonPredict
+  }
+};
+</script>
