062e732a5d834a0bfb6f3d7403d33a7f2fca47d9
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-29 17:04:53 +0700

    Add navbar

--- src/components/HeroTitle.vue
+++ src/components/HeroTitle.vue
@@ -3,8 +3,10 @@
     <section class="hero is-primary">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title">Emotion Detection</h1>
-          <div style="display: flex;">
+          <router-link to="/home" exact class="f-left">
+            <h1 class="title">Emotion Detection</h1>
+          </router-link>
+          <div style="display: flex; margin-top: 1%;">
             <router-link to="/home" exact class="f-left" style="margin-right:1%;">
               <h2 class="subtitle">Home</h2>
             </router-link>
@@ -13,6 +15,9 @@
             </router-link>
             <router-link to="/upload" exact class="f-left" style="margin-right:1%;">
               <h2 class="subtitle">Upload</h2>
+            </router-link>
+            <router-link to="/predict" exact class="f-left" style="margin-right:1%;">
+              <h2 class="subtitle">Predict</h2>
             </router-link>
           </div>
         </div>
