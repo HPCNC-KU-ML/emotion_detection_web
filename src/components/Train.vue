cef511fc3d8c0b066ffdd0842c29305a90e4c779
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-29 17:05:22 +0700

    Add Title

--- src/components/Train.vue
+++ src/components/Train.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="train">
-    <h1>train</h1>
     <div id="number-picker">
+      <h1 class="title">Model preference</h1>
       <b-field>Epoch
         <b-input placeholder="Number" type="number" min="1" max="1000"></b-input>
       </b-field>
@@ -11,7 +11,7 @@
       <b-field>Image size
         <b-input placeholder="Number" type="number" min="1" max="1000"></b-input>
       </b-field>
-      <button class="button is-primary" slot="trigger">Train</button>
+      <button class="button is-primary" slot="trigger">Train Model</button>
     </div>
   </div>
 </template>
@@ -24,6 +24,7 @@ export default {
 
 <style>
 #number-picker {
+  margin-top: 5%;
   margin-left: 30%;
   margin-right: 30%;
 }
