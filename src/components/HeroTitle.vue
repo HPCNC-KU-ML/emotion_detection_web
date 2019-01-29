6a96abc935f2cfa1c898cde28ab3cf43bda74c3b
Jirayu Laungwilawan <jirayu.l@ku.th>
2019-01-29 16:55:21 +0700

    Add tab to go to another page

--- src/components/HeroTitle.vue
+++ src/components/HeroTitle.vue
@@ -4,7 +4,17 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">Emotion Detection</h1>
-          <h2 class="subtitle">Primary subtitle</h2>
+          <div style="display: flex;">
+            <router-link to="/home" exact class="f-left" style="margin-right:1%;">
+              <h2 class="subtitle">Home</h2>
+            </router-link>
+            <router-link to="/train" exact class="f-left" style="margin-right:1%;">
+              <h2 class="subtitle">Train</h2>
+            </router-link>
+            <router-link to="/upload" exact class="f-left" style="margin-right:1%;">
+              <h2 class="subtitle">Upload</h2>
+            </router-link>
+          </div>
         </div>
       </div>
     </section>
